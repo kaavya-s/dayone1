51cbd2b07c91fd14dca5ea1a8971ffbf5f279ac8
kaavya <108513783+kaavya-s@users.noreply.github.com>
2023-03-31 09:32:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Let we see the output of JAVASCRIPT</h1>
+       <button onClick={window['alertMsg']}>Click here</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
